2ccaa3db78355c8265a7766a056eec4e0ed95379
SwimmingLiu <thatdaniel123@outlook.com>
2025-01-06 11:59:17 +0800

    Init HMall 黑马商城初始化

---
+++ swimmingliu-service/src/main/java/com/swimmingliu/service/impl/AddressServiceImpl.java
@@ -0,0 +1,20 @@
+package com.swimmingliu.service.impl;
+
+import com.swimmingliu.domain.po.Address;
+import com.swimmingliu.mapper.AddressMapper;
+import com.swimmingliu.service.IAddressService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2023-05-05
+ */
+@Service
+public class AddressServiceImpl extends ServiceImpl<AddressMapper, Address> implements IAddressService {
+
+}
